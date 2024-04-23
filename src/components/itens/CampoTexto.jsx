6c3d521a7ad194d13301fa91a3f0a728dfc69907
Luciggl <lucielionascimento19@gmail.com>
2024-04-23 16:15:07 -0300

    feat: criando componente CampoTexto

---
+++ src/components/itens/CampoTexto.jsx
@@ -0,0 +1,12 @@
+const CampoTexto = ({tipo,textoExibir, valor, funcao}) => {
+    return(
+        <input 
+        type={tipo}
+        placeholder={textoExibir}
+        value={valor}
+        onChange={(e) => funcao(e.target.value)}
+        />
+    )
+}
+
+export default CampoTexto;
