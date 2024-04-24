da1a11c9b9e37da34ff5f5ab3776646e104ed458
Luciggl <lucielionascimento19@gmail.com>
2024-04-24 10:22:58 -0300

    style: criando css da pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+// import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login';
-import Home from './pages/Home';
-import PrivateRoute from './PrivateRoute'
+// import Home from './pages/Home';
+// import PrivateRoute from './PrivateRoute'
 import './App.css'
 
 const App = () => {
   console.log('Excultando')
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path='/login' Component={Login} />
-      {/* rotas privadas */}
-      <Route element={<PrivateRoute />}>
-        <Route path='/home' Component={Home} />
-      </Route>
-    </Routes>
+    <Login />
+    // <Routes>
+    //   <Route path="/" element={<Navigate to="/login" />} />
+    //   <Route path='/login' Component={Login} />
+    //   {/* rotas privadas */}
+    //   <Route element={<PrivateRoute />}>
+    //     <Route path='/home' Component={Home} />
+    //   </Route>
+    // </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './context/user-context.tsx'
+// import { BrowserRouter } from 'react-router-dom'
+// import { UserProvider } from './context/user-context.tsx'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
+    {/* <UserProvider>
+      <BrowserRouter> */}
         <App />
-      </BrowserRouter>
-    </UserProvider>
+      {/* </BrowserRouter>
+    </UserProvider> */}
   </React.StrictMode>,
 )
