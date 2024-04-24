ab1e7e321d6796de64c076d2e7e833f3eaa020b8
Luciggl <lucielionascimento19@gmail.com>
2024-04-23 22:33:24 -0300

    feat: criando as rotas privadas

---
+++ src/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import React, {useContext} from "react";
+import { UserContext } from "./context/user-context";
+import {Route,Navigate,Outlet} from 'react-router-dom'
+
+
+
+export type PrivateRouteProps = {
+    Children: React.ReactNode;
+    redirectTo: string;
+}
+
+export function PrivateRoute() {
+    const {user} = useContext(UserContext);
+
+    if(!user){
+        return <Navigate to="/login"/>
+    }
+}
