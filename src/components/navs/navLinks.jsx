da1affff61975328d4095b0151d5c171579ec687
Luciggl <lucielionascimento19@gmail.com>
2024-04-26 14:07:59 -0300

    feat: criando e estilizando o componente NavLiks

---
+++ src/components/navs/navLinks.jsx
@@ -0,0 +1,14 @@
+import './navLinks.css'
+
+const NavLink = () => {
+    return(
+        <div className='nav-links-container'>
+            <a href="/home">Home</a>
+            <a href="/error">About</a>
+            <a href="/error">Menu</a>
+            <a href="/error">Pages</a>
+            <a href="/error">Contact</a>
+        </div>
+    )
+}
+export default NavLink;
