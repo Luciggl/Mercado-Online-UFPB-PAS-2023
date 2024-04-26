19d1b1c9c04e7ec56fd9dafbb6993c2ca17c7e65
Luciggl <lucielionascimento19@gmail.com>
2024-04-26 14:32:00 -0300

    fix: App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { Routes, Route, Navigate} from 'react-router-dom'
-import Login from './pages/Login';
+import Login from './pages/Login/Login';
 import Home from './pages/Home';
+import Error from './pages/Error/Error';
+import Cadastro from './pages/Cadastro/Cadastro';
+import ShoppingCard from './pages/ShoppingCart';
 import PrivateRoute from './PrivateRoute'
 import './App.css'
 
 const App = () => {
-  console.log('Excultando')
   return (
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path='/login' Component={Login} />
+        <Route path='/home' Component={Home} />
+        <Route path='/error' Component={Error} />
+        <Route path='/cadastro' Component={Cadastro} />
         {/* rotas privadas */}
         <Route element={<PrivateRoute />}>
-          <Route path='/home' Component={Home} />
+          <Route path='/shop' Component={ShoppingCard} />
         </Route>
       </Routes>
   );
