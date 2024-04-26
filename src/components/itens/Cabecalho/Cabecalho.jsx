4b3cb5111adbdc6ce13086811c38864f38e2c4f9
Luciggl <lucielionascimento19@gmail.com>
2024-04-26 14:34:01 -0300

    feat: Restruturando o cabeçalho

---
+++ src/components/itens/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,46 @@
+import Pesquisa from '../../Pesquisa';
+import logo from '../../../assets/imgs/logo.png';
+import Botao from '../Botao';
+import './Cabecalho.css'
+import { ShoppingBagIcon, MenuIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../../context/user-context';
+import NavLink from '../../navs/navLinks';
+
+const Cabecalho = () => {
+    const navigate = useNavigate();
+    //navegar para pagina de login
+    const handlerLogin = () => {
+        navigate('/login')
+    }
+    //navegar para pagina de carrinho
+    const handlerCarrinho = () => {
+        navigate('/shop')
+    }
+    //navegar para pagina de erro
+    const handlerError = () => {
+        navigate('/error')
+    }
+    const { user } = useContext(UserContext)
+    return (
+        <>
+            <nav className='container-logo'>
+                <div className="logoPesquisa">
+                    <MenuIcon />
+                    <img src={logo} />
+                </div>
+                <div>
+                    <Pesquisa />
+                </div>
+                <Botao classP={'button'} texto={user ? <ShoppingBagIcon /> : 'Login'} funcao={user ? handlerCarrinho : handlerLogin} />
+            </nav>
+            <div>
+                <br />
+                <NavLink />
+            </div>
+        </>
+    )
+}
+
+export default Cabecalho;
