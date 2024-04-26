e49f9a1997aa34a5aa16ada4809fe1598ea09559
Luciggl <lucielionascimento19@gmail.com>
2024-04-26 14:36:33 -0300

    fix: sistema de pesquisa

--- src/components/Pesquisa.jsx
+++ src/components/Pesquisa.jsx
@@ -1,26 +1,38 @@
 import { useState } from "react";
 import Botao from "./itens/Botao";
 import CampoTexto from "./itens/CampoTexto";
+import "./Pesquisa.css";
 
 const Pesquisa = () => {
     const [pesquisaTexto, setPesquisaTexto] = useState('');
-    const [error, setError] = useState('')
+    const [error, setError] = useState('');
 
     const handlerSearch = () => {
         if (!pesquisaTexto.trim()) {
             setError("Por favor, insira o nome do produto desejado");
-            return
-        } else {
-            setError('')
-        }
-    }
+        } else setError('');
+    };
+
     return (
-        <div>
-            <CampoTexto tipo={'text'} textoExibir={'Search'} valor={pesquisaTexto} funcao={setPesquisaTexto} />
-            <Botao texto={'Search'} funcao={handlerSearch} />
-            {error && <div className="error">{error}</div>}
+        <div className="container">
+
+            <div className="pesquisa-container">
+                <CampoTexto
+                    tipo={'text'}
+                    textoExibir={'Search'}
+                    valor={pesquisaTexto}
+                    funcao={setPesquisaTexto}
+                    className="pesquisa-input"
+                />
+                <Botao
+                    classP={'pesquisa-button'}
+                    texto={'Search'}
+                    funcao={handlerSearch}
+                />
+            </div>
+                {error && <div className="error">{error}</div>}
         </div>
-    )
-}
+    );
+};
 
 export default Pesquisa;
