b7973dea6a337ddd28e265bc01278d68da8d04db
Luciggl <lucielionascimento19@gmail.com>
2024-04-29 01:06:15 -0300

    feat: inserindo as imagens na tela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,36 @@
-import { UserContext } from "../context/user-context";
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import Botao from "../components/itens/Botao";
 import Cabecalho from "../components/itens/Cabecalho/Cabecalho.jsx"
 import CardItems from "../components/itens/card-itens/CardsItens.jsx";
-import imagemP from "../assets/imgs/logo.png"
+import AlfaceImg from "../assets/imgs/alface.png"
+import PimentaImg from "../assets/imgs/pimenta.png"
+import BatataImg from "../assets/imgs/batata.png"
+import BerinjelaImg from "../assets/imgs/berinjela.png"
+import CouveFlorImg from "../assets/imgs/colveFlor.png"
+import MacaVerdeImg from "../assets/imgs/macaVerde.png"
+import MangaImg from "../assets/imgs/manga.png"
+import MilhoImg from "../assets/imgs/milho.png"
+import PepinoImg from "../assets/imgs/pepino.png"
+import PimentaoImg from "../assets/imgs/Pimentao.png"
+import PimentaVerdeImg from "../assets/imgs/PimentaVerde.png"
+
 import './Home.css'
 
 const Home = () => {
-    const { user, setUser } = useContext(UserContext);
-    const navigate = useNavigate();
-
-    const handlerSair = () => {
-        const result = window.confirm('Você realmente deseja sair?')
-        if (result) {
-            setUser(null)
-            navigate('/login')
-        }
-    }
-
     return (
         <>
             <Cabecalho />
             <div className="home-container">
-                <CardItems ProdutoImg={imagemP} titulo={'test'} descricao={'sla isso é uma descrição'} valor={12.50} avaliacao={2} />
-                <h2>Home</h2>
-                <p>Olá, {user}</p>
-                <Botao texto={'Sair'} funcao={handlerSair} />
+                <CardItems ProdutoImg={PimentaImg} titulo={'Pimenta'} descricao={'Pimenta vermelha da África do Sul'} valor={3.99} avaliacao={4} />
+                <CardItems ProdutoImg={AlfaceImg} titulo={'Alface'} descricao={'Alface fresca e crocante'} valor={1.99} avaliacao={5} />
+                <CardItems ProdutoImg={BatataImg} titulo={'Batata'} descricao={'Batata branca de ótima qualidade'} valor={2.49} avaliacao={3} />
+                <CardItems ProdutoImg={BerinjelaImg} titulo={'Berinjela'} descricao={'Berinjela roxa de tamanho médio'} valor={2.99} avaliacao={4} />
+                <CardItems ProdutoImg={CouveFlorImg} titulo={'Couve-Flor'} descricao={'Couve-flor fresca e branca'} valor={3.99} avaliacao={5} />
+                <CardItems ProdutoImg={MacaVerdeImg} titulo={'Maçã Verde'} descricao={'Maçã verde fresca e suculenta'} valor={0.99} avaliacao={3} />
+                <CardItems ProdutoImg={MangaImg} titulo={'Manga'} descricao={'Manga madura e doce'} valor={1.49} avaliacao={4} />
+                <CardItems ProdutoImg={MilhoImg} titulo={'Milho'} descricao={'Milho fresco e amarelo'} valor={0.79} avaliacao={3} />
+                <CardItems ProdutoImg={PimentaVerdeImg} titulo={'Pimenta Verde'} descricao={'Pimenta verde doce'} valor={1.99} avaliacao={4} />
+                <CardItems ProdutoImg={PimentaoImg} titulo={'Pimentão'} descricao={'Pimentão vermelho e suculento'} valor={2.99} avaliacao={4} />
             </div>
+
         </>
     );
 };
