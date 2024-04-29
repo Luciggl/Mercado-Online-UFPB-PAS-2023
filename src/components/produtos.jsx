70d60c314dc37eaba9160b372e19a05938cae3be
Luciggl <lucielionascimento19@gmail.com>
2024-04-29 07:26:56 -0300

    feat: criando um componente pros produtos

---
+++ src/components/produtos.jsx
@@ -0,0 +1,25 @@
+import AlfaceImg from "../assets/imgs/alface.png"
+import PimentaImg from "../assets/imgs/pimenta.png"
+import BatataImg from "../assets/imgs/batata.png"
+import BerinjelaImg from "../assets/imgs/berinjela.png"
+import CouveFlorImg from "../assets/imgs/colveFlor.png"
+import MacaVerdeImg from "../assets/imgs/macaVerde.png"
+import MangaImg from "../assets/imgs/manga.png"
+import MilhoImg from "../assets/imgs/milho.png"
+import PimentaoImg from "../assets/imgs/Pimentao.png"
+import PimentaVerdeImg from "../assets/imgs/PimentaVerde.png"
+
+const produtos = [
+    { id: 1, image: PimentaImg, title: 'Pimenta', descricao: 'Pimenta vermelha da África do Sul', valor: 3.99, avaliacao: 4 },
+    { id: 2, image: AlfaceImg, title: 'Alface', descricao: 'Alface fresca e crocante', valor: 1.99, avaliacao: 5 },
+    { id: 3, image: BatataImg, title: 'Batata', descricao: 'Batata branca de ótima qualidade', valor: 2.49, avaliacao: 3 },
+    { id: 4, image: BerinjelaImg, title: 'Berinjela', descricao: 'Berinjela roxa de tamanho médio', valor: 2.99, avaliacao: 4 },
+    { id: 5, image: CouveFlorImg, title: 'Couve-Flor', descricao: 'Couve-flor fresca e branca', valor: 3.99, avaliacao: 5 },
+    { id: 6, image: MacaVerdeImg, title: 'Maçã Verde', descricao: 'Maçã verde fresca e suculenta', valor: 0.99, avaliacao: 3 },
+    { id: 7, image: MangaImg, title: 'Manga', descricao: 'Manga madura e doce', valor: 1.49, avaliacao: 4 },
+    { id: 8, image: MilhoImg, title: 'Milho', descricao: 'Milho fresco e amarelo', valor: 0.79, avaliacao: 3 },
+    { id: 9, image: PimentaVerdeImg, title: 'Pimenta Verde', descricao: 'Pimenta verde doce', valor: 1.99, avaliacao: 4 },
+    { id: 10, image: PimentaoImg, title: 'Pimentão', descricao: 'Pimentão vermelho e suculento', valor: 2.99, avaliacao: 4 }
+];
+
+export default produtos;
